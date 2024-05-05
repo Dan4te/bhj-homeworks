5f17acdc36dc2e2ae1ebb56be4dbe5c94f95f8d2
Dan4te <ilya.burdueff@yandex.ru>
2024-05-05 12:19:16 +0300

    initial commit

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,18 +1,22 @@
 
 let getHole = index => document.getElementById(`hole${index}`);
-let deadMoles = document.getElementById("dead");
-let lostMoles = document.getElementById("lost");
+const deadMoles = document.getElementById("dead");
+const lostMoles = document.getElementById("lost");
 let win = deadMoles.textContent;
 let lost = lostMoles.textContent;
+
 
 function checkGame(deadMoles, lostMoles) {
     console.log(deadMoles, lostMoles);
     if(deadMoles === 10) {
         alert("Вы победили!");
-        location.reload();
+        win = 0;
+        lost = 0;
+
     } else if (lostMoles === 5) {
         alert("Вы проиграли!");
-        location.reload();
+        lost = 0;
+        win = 0;
     }
 }
 
