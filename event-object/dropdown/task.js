fad47846037c8a921620bc32f01211ade89364a3
Dan4te <ilya.burdueff@yandex.ru>
2024-05-09 17:44:22 +0300

    initial commit

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,15 @@
+const dList = document.querySelector('.dropdown__list');
+const dValue = document.querySelector('.dropdown__value');
+const dItem = Array.from(document.querySelectorAll('.dropdown__item'));
+
+dValue.addEventListener('click', () => {
+    dList.classList.add('dropdown__list_active');
+});
+
+dItem.forEach((element) => {
+    element.addEventListener('click', function(event) {
+        dValue.textContent = element.textContent;
+        dList.classList.remove('dropdown__list_active');
+        event.preventDefault();
+    })
+})
